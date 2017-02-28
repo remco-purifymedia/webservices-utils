9be3dc8050a5ed2b9147b68c9a42ccabfae65174
Bas de Mes <bas.de.mes@webservices.nl>
2017-02-28 15:04:09 +0100

    WSNL-1079: Added FileSignatureUtils

---
+++ src/FileSignatureUtils.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace WebservicesNl\Utils;
+
+/**
+ * Class FileSignatureUtil
+ */
+class FileSignatureUtils
+{
+    const PNG = '89504E470D0A1A0A';
+    const JPG = 'FFD8FFE0';
+    const PDF = '25504446';
+    const GIF = '47494638';
+    const BMP = '424D';
+    const XML = '3C3F786D6C';
+
+    /**
+     * Determine whether the given string contains one of the registered signatures
+     *
+     * @param string $string    the string data to check
+     * @param string $signature
+     *
+     * @throws \InvalidArgumentException
+     *
+     * @return bool
+     */
+    public static function hasSignature(string $string, string $signature): bool
+    {
+        if (ctype_xdigit($signature) === false) {
+            throw new \InvalidArgumentException(sprintf('Invalid HEX signature provided: %s', $signature));
+        }
+        $signatureChars = str_split($signature, 2);
+        $hexChars = array_map(
+            function ($val) {
+                return strtoupper(bin2hex($val));
+            },
+            str_split(substr($string, 0, count($signatureChars)))
+        );
+
+        return $hexChars === $signatureChars;
+    }
+}
