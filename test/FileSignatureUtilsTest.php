99010bcfe6b1f0d00d34519dc6e68b1bb6539965
Thijs Alberts <thijs.alberts@webservices.nl>
2017-03-21 11:36:05 +0100

    WSNL-1281: updated tests

--- test/FileSignatureUtilsTest.php
+++ test/FileSignatureUtilsTest.php
@@ -11,13 +11,26 @@ class FileSignatureUtilsTest extends \PHPUnit_Framework_TestCase
 {
     public function testValidPdfSignature()
     {
-        self::assertTrue(FileSignatureUtils::hasSignature(
-            '%PDF We all love chocolate, don\'t we?', FileSignatureUtils::PDF)
+        self::assertTrue(
+            FileSignatureUtils::hasSignature(
+                '%PDF We all love chocolate, don\'t we?',
+                FileSignatureUtils::PDF
+            )
         );
     }
 
     public function testInvalidPdfSignature()
     {
         self::assertFalse(FileSignatureUtils::hasSignature('no PDF', FileSignatureUtils::PDF));
+    }
+
+    public function testInvalidHexSignature()
+    {
+        $hex = 'not_hex_at_all';
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid HEX signature provided: ' . $hex);
+
+        FileSignatureUtils::hasSignature('lalalala', $hex);
     }
 }
